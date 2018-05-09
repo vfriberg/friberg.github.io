876b2cb8c1e3d65c43a9da7b9150162261515466
Victor <victor@homeq.se>
2018-05-09 21:39:36 +0200

    Stuff

---
+++ src/App/App.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <div className={"app"}>
+                <header className={"app-header"}/>
+
+                <p className={"app-intro"}>
+                    Victor Friberg's personal components (and yes, I designed the logo)
+                </p>
+            </div>
+        );
+    }
+}
+
+export default App;
