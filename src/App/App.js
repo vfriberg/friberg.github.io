7c040eae12dd1635e5733d2f09fe9534b9a50090
Victor <victor@homeq.se>
2018-05-09 23:21:43 +0200

    Ejected project, SCSS, grids

--- src/App/App.js
+++ src/App/App.js
@@ -4,11 +4,13 @@ class App extends Component {
     render() {
         return (
             <div className={"app"}>
-                <header className={"app-header"}/>
-
-                <p className={"app-intro"}>
-                    Victor Friberg's personal components (and yes, I designed the logo)
-                </p>
+                <div className={"grid-item item-1"}>Victor</div>
+                <div className={"grid-item"}>Friberg's</div>
+                <div className={"grid-item"}>playground</div>
+                <div className={"grid-item"}>Fresh</div>
+                <div className={"grid-item"}>Stuff</div>
+                <img className={"grid-item"}
+                     src={"https://upload.wikimedia.org/wikipedia/commons/6/66/Economist_Intelligence_Unit_Democracy_index.svg"}/>
             </div>
         );
     }
